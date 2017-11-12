76a1a632ae334ce277db26249f08be3711ac3f20
iurii-kyrylenko <yuriproject@gmail.com>
2017-11-12 21:05:23 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Customization Swagger UI
+
+> npm i
+
+> node_modules/.bin/webpack index.js build/index.js
---
+++ index.js
@@ -0,0 +1,23 @@
+import {
+  SwaggerUIBundle,
+  SwaggerUIStandalonePreset
+ } from 'swagger-ui-dist'
+
+window.onload = function() {
+  
+  // Build a system
+  const ui = SwaggerUIBundle({
+    url: "http://petstore.swagger.io/v2/swagger.json",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  })
+  window.ui = ui
+}
